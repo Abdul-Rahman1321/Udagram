a57cf4bd585aa17278fbbbe6ea48c25af387287b
Abdelrahman Elhosieny <abdelrahman.elhosieny.ae@gmail.com>
2022-04-02 17:59:19 +0200

    co

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-7gug3m99.us-east-1.elasticbeanstalk.com",
+  apiHost: "http://udagram-api-dev.eba-7gug3m99.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
